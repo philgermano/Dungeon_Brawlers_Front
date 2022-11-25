cbded9b67c700578c75f4a327a45cb74f13a8eeb
pgermano <philipgermano@gmail.com>
2022-11-25 16:07:28 -0500

    basic game screen layout

--- src/screens/Game/index.js
+++ src/screens/Game/index.js
@@ -6,9 +6,9 @@
 
 
 import React from "react";
-import { View, Text, ActivityIndicator, StyleSheet } from "react-native";
+import { View, Text, ActivityIndicator, StyleSheet, ScrollView } from "react-native";
 import { StackActions } from "@react-navigation/native";
-
+import {Button} from 'react-native-paper'
 function Game({ navigation }) {
 
 
@@ -16,9 +16,29 @@ function Game({ navigation }) {
 
   return (
     <View style={styles.container}>
-      
-      <Text>Game</Text>
-
+    <View style={styles.gameImage}>
+      <Text>Image</Text>
+      </View>
+      <View style={styles.gameText}>
+    <ScrollView>
+      <Text>TEXT</Text>
+      <Text>TEXT</Text><Text>TEXT</Text><Text>TEXT</Text><Text>TEXT</Text><Text>TEXT</Text><Text>TEXT</Text><Text>TEXT</Text><Text>TEXT</Text><Text>TEXT</Text><Text>TEXT</Text><Text>TEXT</Text><Text>TEXT</Text>
+      </ScrollView>  
+      </View>
+    <View style={styles.controlPanel}>
+      <View style={styles.buttonRow}>
+      <Button  mode="contained" onPress={()=>console.log('pressed')} style={styles.button}>#1 </Button><Button  mode="contained" onPress={()=>console.log('pressed')} style={styles.button}>#2 </Button><Button   mode="contained" onPress={()=>console.log('pressed')} style={styles.button}>#3 </Button>
+      </View>
+      <View style={styles.buttonRow}>
+      <Button  mode="contained" onPress={()=>console.log('pressed')} style={styles.button}>#4 </Button><Button  mode="contained" onPress={()=>console.log('pressed')} style={styles.button}>#5 </Button><Button   mode="contained" onPress={()=>console.log('pressed')} style={styles.button}>#6 </Button>
+      </View>
+      <View style={styles.buttonRow}>
+      <Button  mode="contained" onPress={()=>console.log('pressed')} style={styles.button}>#7 </Button><Button  mode="contained" onPress={()=>console.log('pressed')} style={styles.button}>#8 </Button><Button   mode="contained" onPress={()=>console.log('pressed')} style={styles.button}>#9 </Button>
+      </View>
+      </View>
+      <View style={styles.bottomBar}>
+      <Text>BottomBar, MODAL for menu and help. sound?</Text>
+      </View>
     </View>
   );
 }
@@ -26,9 +46,55 @@ function Game({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+     
   },
+  gameImage: {
+    flex:3,
+    backgroundColor: 'blue',
+    borderColor: 'black',
+    borderWidth: 5,
+    padding: 3,
+  },
+  gameText:{
+    flex: 2,
+    backgroundColor: 'red',
+    borderColor: 'black',
+    borderLeftWidth: 5,
+    borderRightWidth: 5,
+    padding: 3,
+  },
+  controlPanel:{
+    flex:1,
+    backgroundColor: 'green',
+    borderColor: 'black',
+    borderLeftWidth: 5,
+    borderRightWidth: 5,
+    borderTopWidth:5,
+    padding: 3,
+  },
+  bottomBar:{
+    height:50,
+    backgroundColor:'orange',
+    borderColor: 'black',
+    borderWidth: 5,
+    padding: 3,
+  },
+  buttonRow:{
+    flex:1,
+    flexDirection: 'row',
+    width: 'auto',
+    height: 'auto',
+  },
+  button: {
+    flexBasis: 'auto',
+    height: '100%',
+    flexGrow:1,
+    flexShrink:1,
+    borderRadius: 0,
+    borderWidth: 1,
+    borderColor: 'blacks',
+  }
+  
 });
 
 export default Game;
